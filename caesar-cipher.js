0f0526232744a6aa8c408e678bae52c22d7860fc
Metre-Ray <dgary@tut.by>
2020-10-05 23:05:29 +0300

    feat: transform uppercase characters

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -4,12 +4,20 @@ function encode(text, shift) {
     for (let i = 0; i < text.length; i++) {
         const num = text[i].charCodeAt();
         const newNum = num + parseInt(shift);
-        if (num < 97 || num > 122) {
+        if (num >= 65 && num <= 90) {               // encode uppercase english characters
+            if (newNum >= 65 && newNum <= 90) {
+                result += String.fromCharCode(newNum);
+            } else {
+                result += String.fromCharCode(newNum % 90 + 64);
+            } 
+        } else if (num >= 97 && num <= 122) {       //encode lowercase english characters
+            if (newNum >= 97 && newNum <= 122) {
+                result += String.fromCharCode(newNum);
+            } else {
+                result += String.fromCharCode(newNum % 122 + 96);
+            }
+        } else {                                    // don't encode other characters
             result += text[i];
-        } else if (newNum >= 97 && newNum <= 122) {
-            result += String.fromCharCode(newNum);
-        } else {
-            result += String.fromCharCode(newNum % 122 + 96);
         }
     }
     return result;
@@ -21,12 +29,20 @@ function decode(text, shift) {
     for (let i = 0; i < text.length; i++) {
         const num = text[i].charCodeAt();
         const newNum = num - parseInt(shift);
-        if (num < 97 || num > 122) {
+        if (num >= 65 && num <= 90) {               // decode uppercase english characters
+            if (newNum >= 65 && newNum <= 90) {
+                result += String.fromCharCode(newNum);
+            } else {
+                result += String.fromCharCode(90 - (64 - newNum));
+            } 
+        } else if (num >= 97 && num <= 122) {       //decode lowercase english characters
+            if (newNum >= 97 && newNum <= 122) {
+                result += String.fromCharCode(newNum);
+            } else {
+                result += String.fromCharCode(122 - (96 - newNum));
+            }
+        } else {                                    // don't decode other characters
             result += text[i];
-        } else if (newNum >= 97 && newNum <= 122) {
-            result += String.fromCharCode(newNum);
-        } else {
-            result += String.fromCharCode(122 - (96 - newNum));
         }
     }
     return result;
